5772b4324ecfd6c4d703a189c8849009cde24c00
olegtchobotary <olegtchobotary2000@gmail.com>
2019-05-19 13:08:57 +0300

    Changed the database name

--- backend/validation/login.js
+++ backend/validation/login.js
@@ -3,7 +3,7 @@ const isEmpty = require('./is-empty');
 
 module.exports = function validateLoginInput(data) {
     let errors = {};
-    data.email = !isEmpty(data.email) ? data.email : '';
+    data.email = !isEmpty(data.email) ? data.email.toLowerCase() : '';
     data.password = !isEmpty(data.password) ? data.password : '';
 
     if(!Validator.isEmail(data.email)) {
